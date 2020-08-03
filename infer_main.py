32d283a03e918017fe0e440d112b765e3a9a4ac2
NeilDG <fm.atropos@gmail.com>
2020-08-03 21:09:08 +0800

    Testing of syntethic dehazer. Added new jobs to COARE.

--- infer_main.py
+++ infer_main.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 from loaders import dataset_loader
 from trainers import denoise_net_trainer
 from trainers import div2k_trainer
+from model import vanilla_cycle_gan as denoise_gan
 import constants
       
 def infer(batch_size, checkpath, version, iteration):
@@ -48,12 +49,49 @@ def infer(batch_size, checkpath, version, iteration):
         item_number = item_number + 1
         gt.infer(vemon_tensor, item_number)
 
+def dehaze_infer(batch_size, checkpath, version, iteration):
+    device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
+    
+    denoiser = denoise_gan.Generator(n_residual_blocks=3).to(device)
+    denoise_checkpt = torch.load(constants.DENOISE_CHECKPATH)
+    denoiser.load_state_dict(denoise_checkpt[constants.GENERATOR_KEY + "A"])
+    dehazer = denoise_net_trainer.DenoiseTrainer(version, iteration, device, gen_blocks=3)
+    
+    checkpoint = torch.load(checkpath)
+    dehazer.load_saved_state(0, checkpoint, constants.GENERATOR_KEY, constants.DISCRIMINATOR_KEY, constants.OPTIMIZER_KEY)
+ 
+    print("Loaded results checkpt ",checkpath)
+    print("===================================================")
+    
+    dataloader = dataset_loader.load_test_dataset(constants.DATASET_VEMON_PATH, constants.DATASET_CLEAN_PATH, batch_size, -1)
+    
+    # Plot some training images
+    name_batch, dirty_batch, clean_batch = next(iter(dataloader))
+    plt.figure(figsize=constants.FIG_SIZE)
+    plt.axis("off")
+    plt.title("Training - Dirty Images")
+    plt.imshow(np.transpose(vutils.make_grid(dirty_batch.to(device)[:constants.infer_size], nrow = 8, padding=2, normalize=True).cpu(),(1,2,0)))
+    plt.show()
+    
+    plt.figure(figsize=constants.FIG_SIZE)
+    plt.axis("off")
+    plt.title("Training - Clean Images")
+    plt.imshow(np.transpose(vutils.make_grid(clean_batch.to(device)[:constants.infer_size], nrow = 8, padding=2, normalize=True).cpu(),(1,2,0)))
+    plt.show()
+    
+    item_number = 0
+    for i, (name, vemon_batch, gta_batch) in enumerate(dataloader, 0):
+        vemon_tensor = vemon_batch.to(device)
+        item_number = item_number + 1
+        dehazer.dehaze_infer(denoiser, vemon_tensor, item_number)
+
 def main():
-    VERSION = "div2k_denoise_v1.01"
-    ITERATION = "9"
+    VERSION = "dehazing_v1.00"
+    ITERATION = "4"
     CHECKPATH = 'checkpoint/' + VERSION + "_" + ITERATION +'.pt'
     
-    infer(constants.infer_size, CHECKPATH, VERSION, ITERATION)
+    dehaze_infer(constants.infer_size, CHECKPATH, VERSION, ITERATION)
+    #infer(constants.infer_size, CHECKPATH, VERSION, ITERATION)
 
 #FIX for broken pipe num_workers issue.
 if __name__=="__main__": 
