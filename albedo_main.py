ddb4fca9c4b8d8b304e63474f03300afcb7081da
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2021-05-04 20:02:36 +0800

    Refactored and created albedo trainer from CycleGAN trainer.

---
+++ albedo_main.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+"""
+Main entry for GAN training
+Created on Sun Apr 19 13:22:06 2020
+
+@author: delgallegon
+"""
+
+from __future__ import print_function
+import sys
+from optparse import OptionParser
+import random
+import torch
+import torch.nn.parallel
+import torch.utils.data
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+from loaders import dataset_loader
+from trainers import albedo_trainer
+import constants
+     
+parser = OptionParser()
+parser.add_option('--coare', type=int, help="Is running on COARE?", default=0)
+parser.add_option('--img_to_load', type=int, help="Image to load?", default=-1)
+parser.add_option('--load_previous', type=int, help="Load previous?", default=0)
+parser.add_option('--iteration', type=int, help="Style version?", default="1")
+parser.add_option('--identity_weight', type=float, help="Weight", default="0.0")
+parser.add_option('--adv_weight', type=float, help="Weight", default="1.0")
+parser.add_option('--likeness_weight', type=float, help="Weight", default="10.0")
+parser.add_option('--psnr_loss_weight', type=float, help="Weight", default="0.0")
+parser.add_option('--use_psnr', type=int, help="LR", default="1")
+parser.add_option('--cycle_weight', type=float, help="Weight", default="0.0")
+parser.add_option('--g_lr', type=float, help="LR", default="0.0002")
+parser.add_option('--d_lr', type=float, help="LR", default="0.0002")
+parser.add_option('--comments', type=str, help="comments for bookmarking", default = "Vanilla CycleGAN. Paired learning for extracting albedo from a lit image and vice versa. \n"
+                                                                                     "Now uses hazy images as input for extracing albedo. \n"
+                                                                                     "Uses S(x) = A(x) + L(x) formula" )
+
+#--img_to_load=-1 --load_previous=1
+#Update config if on COARE
+def update_config(opts):
+    constants.is_coare = opts.coare
+    
+    if(constants.is_coare == 1):
+        print("Using COARE configuration.")
+        constants.batch_size = constants.batch_size * 2
+        
+        constants.ITERATION = str(opts.iteration)
+        constants.COLOR_TRANSFER_CHECKPATH = 'checkpoint/' + constants.COLOR_TRANSFER_VERSION + "_" + constants.ITERATION + '.pt'
+
+        constants.DATASET_CLEAN_PATH_COMPLETE_STYLED_3 = "/scratch1/scratch2/neil.delgallego/Synth Hazy 3/clean/"
+        constants.DATASET_ALBEDO_PATH_COMPLETE_3 = "/scratch1/scratch2/neil.delgallego/Synth Hazy 3/albedo/"
+        constants.DATASET_DEPTH_PATH_COMPLETE_3 = "/scratch1/scratch2/neil.delgallego/Synth Hazy 3/depth/"
+        constants.DATASET_OHAZE_HAZY_PATH_COMPLETE = "/scratch1/scratch2/neil.delgallego/Hazy Dataset Benchmark/O-HAZE/hazy/"
+
+        constants.DATASET_ALBEDO_PATH_PATCH_3 = "/scratch1/scratch2/neil.delgallego/Synth Hazy 3 - Patch/albedo/"
+        constants.DATASET_ALBEDO_PATH_PSEUDO_PATCH_3 = "/scratch1/scratch2/neil.delgallego/Synth Hazy 3 - Patch/albedo - pseudo/"
+        constants.DATASET_CLEAN_PATH_PATCH_STYLED_3 = "/scratch1/scratch2/neil.delgallego/Synth Hazy 3 - Patch/clean - styled/"
+
+def show_images(img_tensor, caption):
+    device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
+    plt.figure(figsize=constants.FIG_SIZE)
+    plt.axis("off")
+    plt.title(caption)
+    plt.imshow(np.transpose(vutils.make_grid(img_tensor.to(device)[:constants.batch_size], nrow = 8, padding=2, normalize=True).cpu(),(1,2,0)))
+    plt.show()
+
+def main(argv):
+    (opts, args) = parser.parse_args(argv)
+    update_config(opts)
+    print("=====================BEGIN============================")
+    print("Is Coare? %d Has GPU available? %d Count: %d" % (constants.is_coare, torch.cuda.is_available(), torch.cuda.device_count()))
+    print("Torch CUDA version: %s" % torch.version.cuda)
+    
+    manualSeed = random.randint(1, 10000) # use if you want new results
+    random.seed(manualSeed)
+    torch.manual_seed(manualSeed)
+    
+    device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
+    print("Device: %s" % device)
+    
+    gt = albedo_trainer.AlbedoTrainer(device, opts.g_lr, opts.d_lr)
+    gt.update_penalties(opts.adv_weight, opts.identity_weight, opts.likeness_weight, opts.cycle_weight, opts.psnr_loss_weight, opts.use_psnr, opts.comments)
+    start_epoch = 0
+    iteration = 0
+    
+    if(opts.load_previous): 
+        checkpoint = torch.load(constants.COLOR_TRANSFER_CHECKPATH)
+        start_epoch = checkpoint['epoch'] + 1   
+        iteration = checkpoint['iteration'] + 1
+        gt.load_saved_state(checkpoint)
+ 
+        print("Loaded checkpt: %s Current epoch: %d" % (constants.COLOR_TRANSFER_CHECKPATH, start_epoch))
+        print("===================================================")
+    
+    # Create the dataloader
+    train_loader = dataset_loader.load_color_albedo_train_dataset(constants.DATASET_CLEAN_PATH_PATCH_STYLED_3, constants.DATASET_ALBEDO_PATH_PATCH_3, constants.DATASET_DEPTH_PATH_COMPLETE_3, constants.batch_size, opts.img_to_load)
+    test_loader_1 = dataset_loader.load_color_albedo_test_dataset(constants.DATASET_CLEAN_PATH_COMPLETE_STYLED_3, constants.DATASET_ALBEDO_PATH_COMPLETE_3, constants.DATASET_DEPTH_PATH_COMPLETE_3, constants.batch_size, 500)
+    test_loader_2 = dataset_loader.load_color_albedo_test_dataset(constants.DATASET_OHAZE_HAZY_PATH_COMPLETE, constants.DATASET_ALBEDO_PATH_COMPLETE_3, None, constants.batch_size, 500)
+    index = 0
+    
+    # Plot some training images
+    if(constants.is_coare == 0):
+        _, noisy_batch, clean_batch = next(iter(train_loader))
+
+        show_images(noisy_batch, "Training - A Images")
+        show_images(clean_batch, "Training - B Images")
+    
+    print("Starting Training Loop...")
+    for epoch in range(start_epoch, constants.num_epochs):
+        # For each batch in the dataloader
+        for i, train_data in enumerate(train_loader, 0):
+            _, dirty_batch, clean_batch = train_data
+            dirty_tensor = dirty_batch.to(device)
+            clean_tensor = clean_batch.to(device)
+
+            gt.train(dirty_tensor, clean_tensor)
+            if(i % 5000 == 0):
+                view_batch, view_dirty_batch, view_clean_batch = next(iter(test_loader_1))
+                view_dirty_batch = view_dirty_batch.to(device)
+                view_clean_batch = view_clean_batch.to(device)
+                gt.visdom_report(iteration, dirty_tensor, clean_tensor, view_dirty_batch, view_clean_batch)
+
+                view_batch, view_dirty_batch, _ = next(iter(test_loader_2))
+                view_dirty_batch = view_dirty_batch.to(device)
+                gt.visdom_infer(view_dirty_batch, "O-Haze Hazy", "O-Haze Albedo")
+
+                iteration = iteration + 1
+                index = (index + 1) % len(test_loader_1)
+                if(index == 0):
+                    test_loader_1 = dataset_loader.load_color_albedo_test_dataset(constants.DATASET_CLEAN_PATH_COMPLETE_STYLED_3, constants.DATASET_ALBEDO_PATH_COMPLETE_3, constants.DATASET_DEPTH_PATH_COMPLETE_3, constants.batch_size, 500)
+                    test_loader_2 = dataset_loader.load_color_albedo_test_dataset(constants.DATASET_OHAZE_HAZY_PATH_COMPLETE, constants.DATASET_ALBEDO_PATH_COMPLETE_3, None, constants.batch_size, 500)
+
+                gt.save_states(epoch, iteration)
+
+#FIX for broken pipe num_workers issue.
+if __name__=="__main__": 
+    main(sys.argv)
+
---
+++ trainers/albedo_trainer.py
@@ -0,0 +1,263 @@
+# -*- coding: utf-8 -*-
+# Template trainer. Do not use this for actual training.
+
+import itertools
+import os
+
+import kornia
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.cuda.amp as amp
+import torch.nn as nn
+import torchvision.utils as vutils
+import constants
+from model import vanilla_cycle_gan as cycle_gan
+from utils import plot_utils
+
+
+class AlbedoTrainer:
+
+    def __init__(self, gpu_device, g_lr, d_lr):
+        self.gpu_device = gpu_device
+        self.g_lr = g_lr
+        self.d_lr = d_lr
+        self.G_A = cycle_gan.Generator(downsampling_blocks = 2, n_residual_blocks=16).to(self.gpu_device)
+        self.D_A = cycle_gan.Discriminator().to(self.gpu_device)  # use CycleGAN's discriminator
+
+        self.visdom_reporter = plot_utils.VisdomReporter()
+        self.optimizerG = torch.optim.Adam(itertools.chain(self.G_A.parameters()), lr=self.g_lr)
+        self.optimizerD = torch.optim.Adam(itertools.chain(self.D_A.parameters()), lr=self.d_lr)
+        self.schedulerG = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizerG, patience=100000 / constants.batch_size, threshold=0.00005)
+        self.schedulerD = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizerD, patience=100000 / constants.batch_size, threshold=0.00005)
+        self.initialize_dict()
+
+        self.fp16_scaler = amp.GradScaler()  # for automatic mixed precision
+
+    def initialize_dict(self):
+        # what to store in visdom?
+        self.losses_dict = {}
+        self.losses_dict[constants.G_LOSS_KEY] = []
+        self.losses_dict[constants.D_OVERALL_LOSS_KEY] = []
+        self.losses_dict[constants.IDENTITY_LOSS_KEY] = []
+        self.losses_dict[constants.LIKENESS_LOSS_KEY] = []
+        self.losses_dict[constants.PSNR_LOSS_KEY] = []
+        self.losses_dict[constants.G_ADV_LOSS_KEY] = []
+        self.losses_dict[constants.D_A_FAKE_LOSS_KEY] = []
+        self.losses_dict[constants.D_A_REAL_LOSS_KEY] = []
+
+        self.caption_dict = {}
+        self.caption_dict[constants.G_LOSS_KEY] = "G loss per iteration"
+        self.caption_dict[constants.D_OVERALL_LOSS_KEY] = "D loss per iteration"
+        self.caption_dict[constants.IDENTITY_LOSS_KEY] = "Identity loss per iteration"
+        self.caption_dict[constants.LIKENESS_LOSS_KEY] = "Likeness loss per iteration"
+        self.caption_dict[constants.PSNR_LOSS_KEY] = "PSNR/SSIM color loss per iteration"
+        self.caption_dict[constants.G_ADV_LOSS_KEY] = "G adv loss per iteration"
+        self.caption_dict[constants.D_A_FAKE_LOSS_KEY] = "D(A) fake loss per iteration"
+        self.caption_dict[constants.D_A_REAL_LOSS_KEY] = "D(A) real loss per iteration"
+
+    def update_penalties(self, adv_weight, id_weight, likeness_weight, cycle_weight, psnr_loss_weight, use_psnr, comments):
+        # what penalties to use for losses?
+        self.adv_weight = adv_weight
+        self.id_weight = id_weight
+        self.likeness_weight = likeness_weight
+        self.cycle_weight = cycle_weight
+        self.psnr_loss_weight = psnr_loss_weight
+        self.use_psnr = use_psnr
+
+        # save hyperparameters for bookeeping
+        HYPERPARAMS_PATH = "checkpoint/" + constants.COLOR_TRANSFER_VERSION + "_" + constants.ITERATION + ".config"
+        with open(HYPERPARAMS_PATH, "w") as f:
+            print("Version: ", constants.COLOR_TRANSFER_CHECKPATH, file=f)
+            print("Comment: ", comments, file=f)
+            print("Learning rate for G: ", str(self.g_lr), file=f)
+            print("Learning rate for D: ", str(self.d_lr), file=f)
+            print("====================================", file=f)
+            print("Adv weight: ", str(self.adv_weight), file=f)
+            print("Identity weight: ", str(self.id_weight), file=f)
+            print("Likeness weight: ", str(self.likeness_weight), file=f)
+            print("Use PSNR: ", str(self.use_psnr), file = f)
+            print("PSNR/SSIM loss weight: ", str(self.psnr_loss_weight), file=f)
+            print("Cycle weight: ", str(self.cycle_weight), file=f)
+            print("====================================", file=f)
+            print("Brightness enhance: ", str(constants.brightness_enhance), file=f)
+            print("Contrast enhance: ", str(constants.contrast_enhance), file=f)
+
+    def adversarial_loss(self, pred, target):
+        loss = nn.L1Loss()
+        return loss(pred, target)
+
+    def identity_loss(self, pred, target):
+        loss = nn.L1Loss()
+        return loss(pred, target)
+
+    def cycle_loss(self, pred, target):
+        loss = nn.L1Loss()
+        return loss(pred, target)
+
+    def likeness_loss(self, pred, target):
+        loss = nn.L1Loss()
+        return loss(pred, target)
+
+    def smoothness_loss(self, pred, target):
+        loss = nn.L1Loss()
+        pred_blur = kornia.gaussian_blur2d(pred, (7, 7), (5.5, 5.5))
+        target_blur = kornia.gaussian_blur2d(target, (7, 7), (5.5, 5.5))
+
+        return loss(pred_blur, target_blur)
+
+    def psnr_loss(self, pred, target):
+        if(self.use_psnr):
+            loss = kornia.losses.PSNRLoss(max_val=0.5)
+        else:
+            loss = kornia.losses.SSIMLoss(window_size=3, max_val=0.5)
+
+        return loss(pred, target)
+
+    def train(self, styled_tensor, albedo_tensor):
+        with amp.autocast():
+            albedo_like = self.G_A(styled_tensor)
+
+            self.D_A.train()
+            self.optimizerD.zero_grad()
+
+            prediction = self.D_A(albedo_tensor)
+            real_tensor = torch.ones_like(prediction)
+            fake_tensor = torch.zeros_like(prediction)
+
+            D_A_real_loss = self.adversarial_loss(self.D_A(albedo_tensor), real_tensor) * self.adv_weight
+            D_A_fake_loss = self.adversarial_loss(self.D_A(albedo_like.detach()), fake_tensor) * self.adv_weight
+
+            errD = D_A_real_loss + D_A_fake_loss
+            self.fp16_scaler.scale(errD).backward()
+            self.fp16_scaler.step(self.optimizerD)
+            self.schedulerD.step(errD)
+
+            self.G_A.train()
+            self.optimizerG.zero_grad()
+
+            identity_like = self.G_A(albedo_tensor)
+            albedo_like = styled_tensor - self.G_A(styled_tensor)
+
+            identity_loss = self.identity_loss(identity_like, albedo_tensor) * self.id_weight
+            A_likeness_loss = self.likeness_loss(albedo_like, albedo_tensor) * self.likeness_weight
+            A_color_loss = self.psnr_loss(albedo_like, albedo_tensor) * self.psnr_loss_weight
+
+            prediction = self.D_A(albedo_like)
+            real_tensor = torch.ones_like(prediction)
+            A_adv_loss = self.adversarial_loss(prediction, real_tensor) * self.adv_weight
+
+            errG = identity_loss + A_likeness_loss + A_color_loss + A_adv_loss
+
+            self.fp16_scaler.scale(errG).backward()
+            self.fp16_scaler.step(self.optimizerG)
+            self.schedulerG.step(errG)
+
+            self.fp16_scaler.update()
+
+        # what to put to losses dict for visdom reporting?
+        self.losses_dict[constants.G_LOSS_KEY].append(errG.item())
+        self.losses_dict[constants.D_OVERALL_LOSS_KEY].append(errD.item())
+        self.losses_dict[constants.IDENTITY_LOSS_KEY].append(identity_loss.item())
+        self.losses_dict[constants.LIKENESS_LOSS_KEY].append(A_likeness_loss.item())
+        self.losses_dict[constants.PSNR_LOSS_KEY].append(A_color_loss.item())
+        self.losses_dict[constants.G_ADV_LOSS_KEY].append(A_adv_loss.item())
+        self.losses_dict[constants.D_A_FAKE_LOSS_KEY].append(D_A_fake_loss.item())
+        self.losses_dict[constants.D_A_REAL_LOSS_KEY].append(D_A_real_loss.item())
+
+    def visdom_report(self, iteration, styled_tensor, albedo_tensor, test_styled_tensor, test_albedo_tensor):
+        with torch.no_grad():
+            albedo_like = styled_tensor - self.G_A(styled_tensor)
+            test_albedo_like = test_styled_tensor - self.G_A(test_styled_tensor)
+
+        # report to visdom
+        self.visdom_reporter.plot_finegrain_loss(str(constants.COLOR_TRANSFER_VERSION) +str(constants.ITERATION), iteration, self.losses_dict, self.caption_dict)
+        self.visdom_reporter.plot_image(styled_tensor, "Training Dirty images - " + str(constants.COLOR_TRANSFER_VERSION) + str(constants.ITERATION))
+        self.visdom_reporter.plot_image(albedo_tensor, "Training Clean images - " + str(constants.COLOR_TRANSFER_VERSION) + str(constants.ITERATION))
+        self.visdom_reporter.plot_image(albedo_like, "Training Clean-like images - "+str(constants.COLOR_TRANSFER_VERSION) +str(constants.ITERATION))
+        self.visdom_reporter.plot_image(test_styled_tensor, "Test Dirty images - " + str(constants.COLOR_TRANSFER_VERSION) + str(constants.ITERATION))
+        self.visdom_reporter.plot_image(test_albedo_tensor, "Test Clean images - " + str(constants.COLOR_TRANSFER_VERSION) + str(constants.ITERATION))
+        self.visdom_reporter.plot_image(test_albedo_like, "Test Clean-like images - "+str(constants.COLOR_TRANSFER_VERSION) +str(constants.ITERATION))
+
+    def visdom_infer(self, test_styled_tensor, caption_dirty, caption_clean):
+        with torch.no_grad():
+            test_clean_like = test_styled_tensor - self.G_A(test_styled_tensor)
+
+        self.visdom_reporter.plot_image(test_styled_tensor, caption_dirty + " - " + str(constants.COLOR_TRANSFER_VERSION) + str(constants.ITERATION))
+        self.visdom_reporter.plot_image(test_clean_like, caption_clean + " - "+str(constants.COLOR_TRANSFER_VERSION) +str(constants.ITERATION))
+
+    def produce_image(self, dirty_tensor):
+        with torch.no_grad():
+            clean_like = self.G_A(dirty_tensor, self.denoise_model(dirty_tensor))
+            resized_fake = nn.functional.interpolate(clean_like, scale_factor=1.0, mode="bilinear", recompute_scale_factor=True)
+
+        return resized_fake
+
+    def infer(self, dirty_tensor, file_number):
+        LOCATION = os.getcwd() + "/figures/"
+        with torch.no_grad():
+            clean_like = self.G_A(dirty_tensor).detach()
+
+        # resize tensors for better viewing
+        resized_normal = nn.functional.interpolate(dirty_tensor, scale_factor=4.0, mode="bilinear", recompute_scale_factor=True)
+        resized_fake = nn.functional.interpolate(clean_like, scale_factor=4.0, mode="bilinear", recompute_scale_factor=True)
+
+        print("New shapes: %s %s" % (np.shape(resized_normal), np.shape(resized_fake)))
+
+        fig, ax = plt.subplots(2, 1)
+        fig.set_size_inches((32, 16))
+        fig.tight_layout()
+
+        ims = np.transpose(vutils.make_grid(resized_normal, nrow=8, padding=2, normalize=True).cpu(), (1, 2, 0))
+        ax[0].set_axis_off()
+        ax[0].imshow(ims)
+
+        ims = np.transpose(vutils.make_grid(resized_fake, nrow=8, padding=2, normalize=True).cpu(), (1, 2, 0))
+        ax[1].set_axis_off()
+        ax[1].imshow(ims)
+
+        plt.subplots_adjust(left=0.06, wspace=0.0, hspace=0.15)
+        plt.savefig(LOCATION + "result_" + str(file_number) + ".png")
+        plt.show()
+
+    def load_saved_state(self, checkpoint):
+        self.G_A.load_state_dict(checkpoint[constants.GENERATOR_KEY + "A"])
+        self.D_A.load_state_dict(checkpoint[constants.DISCRIMINATOR_KEY + "A"])
+        self.optimizerG.load_state_dict(checkpoint[constants.GENERATOR_KEY + constants.OPTIMIZER_KEY])
+        self.optimizerD.load_state_dict(checkpoint[constants.DISCRIMINATOR_KEY + constants.OPTIMIZER_KEY])
+        self.schedulerG.load_state_dict(checkpoint[constants.GENERATOR_KEY + "scheduler"])
+        self.schedulerD.load_state_dict(checkpoint[constants.DISCRIMINATOR_KEY + "scheduler"])
+
+    def save_states(self, epoch, iteration):
+        save_dict = {'epoch': epoch, 'iteration': iteration}
+        netGA_state_dict = self.G_A.state_dict()
+        netDA_state_dict = self.D_A.state_dict()
+
+        optimizerG_state_dict = self.optimizerG.state_dict()
+        optimizerD_state_dict = self.optimizerD.state_dict()
+
+        schedulerG_state_dict = self.schedulerG.state_dict()
+        schedulerD_state_dict = self.schedulerD.state_dict()
+
+        save_dict[constants.GENERATOR_KEY + "A"] = netGA_state_dict
+        save_dict[constants.DISCRIMINATOR_KEY + "A"] = netDA_state_dict
+
+        save_dict[constants.GENERATOR_KEY + constants.OPTIMIZER_KEY] = optimizerG_state_dict
+        save_dict[constants.DISCRIMINATOR_KEY + constants.OPTIMIZER_KEY] = optimizerD_state_dict
+
+        save_dict[constants.GENERATOR_KEY + "scheduler"] = schedulerG_state_dict
+        save_dict[constants.DISCRIMINATOR_KEY + "scheduler"] = schedulerD_state_dict
+
+        torch.save(save_dict, constants.COLOR_TRANSFER_CHECKPATH)
+        print("Saved model state: %s Epoch: %d" % (len(save_dict), (epoch + 1)))
+
+        # clear plots to avoid potential sudden jumps in visualization due to unstable gradients during early training
+        if (epoch % 20 == 0):
+            self.losses_dict[constants.G_LOSS_KEY].clear()
+            self.losses_dict[constants.D_OVERALL_LOSS_KEY].clear()
+            self.losses_dict[constants.IDENTITY_LOSS_KEY].clear()
+            self.losses_dict[constants.LIKENESS_LOSS_KEY].clear()
+            self.losses_dict[constants.PSNR_LOSS_KEY].clear()
+            self.losses_dict[constants.G_ADV_LOSS_KEY].clear()
+            self.losses_dict[constants.D_A_FAKE_LOSS_KEY].clear()
+            self.losses_dict[constants.D_A_REAL_LOSS_KEY].clear()
