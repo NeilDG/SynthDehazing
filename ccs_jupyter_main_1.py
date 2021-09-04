0f3bd61e52d7c170398cfb0d0429f2adb6f5823d
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2021-09-04 12:30:33 +0800

    Bug fixes and optimization

--- ccs_jupyter_main_1.py
+++ ccs_jupyter_main_1.py
@@ -1,10 +1,7 @@
 import os
-import transmission_main
 def main():
-    # os.system("python \"transmission_main.py\" --num_workers=10 --img_to_load=-1 --load_previous=0 --server_config=2 --cuda_device=cuda:0 --batch_size=512 --iteration=8")
-    # os.system("python \"transmission_main.py\" --num_workers=10 --img_to_load=-1 --load_previous=0 --server_config=2 --cuda_device=cuda:0 --batch_size=512 --iteration=9")
-    os.system("python \"transmission_main.py\" --num_workers=10 --img_to_load=-1 --load_previous=1 --iteration=1 --server_config=2 --batch_size=64 --patch_size=128")
-    os.system("python \"airlight_main.py\" --num_workers=10 --img_to_load=-1 --load_previous=1 --iteration=1 --server_config=2 --batch_size=4096 --patch_size=128")
+    os.system("python \"transmission_main.py\" --num_workers=10 --img_to_load=-1 --load_previous=0 --iteration=4 --server_config=2 --batch_size=512 --t_min=0.6 --t_max=1.8 --a_min=0.7 --a_max=1.0")
+    os.system("python \"airlight_main.py\" --num_workers=10 --img_to_load=-1 --load_previous=0 --iteration=4 --server_config=2 --batch_size=32768 --t_min=0.6 --t_max=1.8 --a_min=0.7 --a_max=1.0")
 
 if __name__ == "__main__":
     main()
--- pc_main.py
+++ pc_main.py
@@ -1,7 +1,7 @@
 import os
 def main():
-    # os.system("python \"transmission_main.py\" --num_workers=12 --img_to_load=-1 --load_previous=1 --iteration=2 --batch_size=128 --patch_size=32 --a_min=0.1 --a_max=0.98")
-    os.system("python \"airlight_main.py\" --num_workers=12 --img_to_load=-1 --load_previous=1 --iteration=2 --batch_size=16384 --patch_size=32 --a_min=0.1 --a_max=0.98")
+    # os.system("python \"transmission_main.py\" --num_workers=12 --img_to_load=-1 --load_previous=1 --iteration=3 --batch_size=128 --patch_size=32 --a_min=0.1 --a_max=0.98")
+    os.system("python \"airlight_main.py\" --num_workers=12 --img_to_load=-1 --load_previous=1 --iteration=3 --batch_size=4096 --patch_size=32 --a_min=0.1 --a_max=0.98")
 
 if __name__ == "__main__":
     main()
